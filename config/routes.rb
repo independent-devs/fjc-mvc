5abd489cda6a97d5cb7e67d198374fbb07be0ae9
felix1251 <domhack123@gmail.com>
2024-03-01 01:39:08 +0800

    separate routes using draw

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
 
   root "home#index"
-  resources :products
 
-  ## only show admin routes if user is authenticated and is admin
-  authenticated :user, -> { _1.admin } do
-    namespace :admin, path: '/admin' do
-      root 'home#index'
-      resources :products
-    end
-  end
+  ## draw routes
+  draw :products
+  draw :admin
 end
---
+++ config/routes/admin.rb
@@ -0,0 +1,7 @@
+## only show admin routes if user is authenticated and is admin
+authenticated :user, -> { _1.admin } do
+  namespace :admin, path: '/admin' do
+    root 'home#index'
+    resources :products
+  end
+end
