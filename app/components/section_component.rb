cedaa266e53336d8ff61742ad2f17aeb731f3f30
felix1251 <domhack123@gmail.com>
2024-03-01 20:41:10 +0800

    category section action partial

--- app/components/section_component.rb
+++ app/components/section_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class SectionComponent < ViewComponent::Base
-  attr_reader :label, :title
+  attr_reader :label, :title, :action_partial
 
-  def initialize(label:, title:)
+  def initialize(label:, title:, action_partial: '')
     @label = label
     @title = title
+    @action_partial = action_partial
     super
   end
 end
