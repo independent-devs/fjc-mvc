edb70e145ca79ca5d4a4e25408163794b6f5caad
felix1251 <domhack123@gmail.com>
2024-03-06 02:04:20 +0800

    ..

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,8 +5,7 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.base_on_date.has_captured_price
-                       .not_deleted.sort_by_latest
+    @products = Product.base_on_date.has_captured_price.not_deleted.sort_by_latest
   end
 
   # GET /products/1 or /products/1.json
