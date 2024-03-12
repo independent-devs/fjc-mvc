e70f3e8fd1ad194063863502b6fe6a2cfb233166
felix1251 <domhack123@gmail.com>
2024-03-13 05:32:23 +0800

    add parent to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,9 @@
 
 class Category < ApplicationRecord
   has_many :product_categories, dependent: :destroy
+
+  belongs_to :parent, class_name: 'Category', optional: true
+  has_many :subcategories, class_name: 'Category', foreign_key: :parent_id, dependent: :destroy, inverse_of: :category
 end
 
 # == Schema Information
@@ -14,9 +17,15 @@ end
 #  position   :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  parent_id  :bigint
 #
 # Indexes
 #
 #  index_categories_on_deleted_at  (deleted_at)
+#  index_categories_on_parent_id   (parent_id)
 #  index_categories_on_position    (position)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (parent_id => categories.id)
 #
