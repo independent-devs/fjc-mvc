126e5d5ce1a844f11edf3b8a27302ec060c135b0
felix1251 <domhack123@gmail.com>
2024-03-12 21:58:16 +0800

    add sortable importmap

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,28 @@
+import Sortable from "stimulus-sortable";
+
+export default class extends Sortable {
+  connect() {
+    super.connect();
+
+    // The sortable.js instance.
+    this.sortable;
+
+    // Your options
+    this.options;
+
+    // Your default options
+    this.defaultOptions;
+  }
+
+  // You can override the `onUpdate` method here.
+  onUpdate(event) {
+    super.onUpdate(event);
+  }
+
+  // You can set default options in this getter for all sortable elements.
+  get defaultOptions() {
+    return {
+      animation: 500,
+    };
+  }
+}
