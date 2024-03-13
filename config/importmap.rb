1e1e641701eced49ff944c2afe1e5b689c58d8bc
felix1251 <domhack123@gmail.com>
2024-03-13 21:19:17 +0800

    make js controller lazyload

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,9 +4,9 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/controllers", under: "controllers", preload: false
 
 pin 'intl-tel-input', to: 'https://ga.jspm.io/npm:intl-tel-input@18.2.1/index.js'
-pin "stimulus-sortable" # @4.1.1
-pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.8
-pin "sortablejs" # @1.15.2
+pin "stimulus-sortable", preload: false # @4.1.1
+pin "@rails/request.js", to: "@rails--request.js.js", preload: false # @0.0.8
+pin "sortablejs", preload: false # @1.15.2
