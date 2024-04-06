7a6caa65051c4a285438ae32cc6d2eafdeb326b6
felix1251 <domhack123@gmail.com>
2024-04-07 00:51:17 +0800

    cache seo and description inside product model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,11 +9,6 @@ class ProductsController < ApplicationController
   # GET /products/black-shoes?pid=34b404d1-8104-402a-a25a-30e831712b7a
   def show
     @product = Product.single_public(params[:slug], params[:pid])
-
-    # seo low level cache
-    @seo = Rails.cache.fetch(['Products', @product.id, 'seo', @product.updated_at.to_i], expires_in: 12.hours) do
-      @product.seo
-    end
   rescue ActiveRecord::RecordNotFound
     render 'not_found', status: :not_found
   end
