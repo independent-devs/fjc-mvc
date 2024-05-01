09ff17d098058ab397430b5fe09e5a6e24e5dd79
felix1251 <domhack123@gmail.com>
2024-05-01 12:13:51 +0800

    stock js controller comment

--- app/javascript/controllers/stock_controller.js
+++ app/javascript/controllers/stock_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="modify-stock"
+// Connects to data-controller="stock"
 export default class extends Controller {
   static targets = ["input", "action"];
 
