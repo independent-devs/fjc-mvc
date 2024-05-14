fb4cabc5511df15180b04103c85565a41fdd91bb
felix1251 <domhack123@gmail.com>
2024-05-15 03:01:40 +0800

    public product js getting common variant

--- app/javascript/controllers/product/public_controller.js
+++ app/javascript/controllers/product/public_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="product--public"
 export default class extends Controller {
-  static targets = ["options", "addToCartBtn", "buyNowBtn"]
+  static targets = ["option", "options", "addToCartBtn", "buyNowBtn"]
 
   radioToggle(event) {
     const radioName = event.target.name
@@ -14,11 +14,12 @@ export default class extends Controller {
     if (event.target.dataset.wasChecked == "true") {
       // set radio with the event element
       this.setRadios(radioName, radioID, null)
-      return
+    } else {
+      event.target.checked = true
+      event.target.dataset.wasChecked = true
     }
 
-    event.target.checked = true
-    event.target.dataset.wasChecked = true
+    console.log(this.commonVariant)
   }
 
   setRadios(name, includeId, excludeId) {
@@ -27,9 +28,51 @@ export default class extends Controller {
     if (includeId) query += `[id="${includeId}"]`
     if (excludeId) query += `:not([id="${excludeId}"]`
 
-    this.optionsTarget.querySelectorAll(query).forEach((element) => {
-      element.checked = false
-      element.dataset.wasChecked = false
+    this.optionsTarget.querySelectorAll(query).forEach((el) => {
+      el.checked = false
+      el.dataset.wasChecked = false
     })
+  }
+
+  get commonVariant() {
+    const collection = this.radioCollections;
+
+    if (!collection.length) return null;
+
+    const duplicates = [];
+    const frequencyMap = {};
+
+    for (let i = 0; i < collection.length; i++) {
+      const element = collection[i];
+
+      if (frequencyMap[element]) {
+        if (!duplicates.includes(element)) {
+          duplicates.push(element);
+          break
+        }
+      } else {
+        frequencyMap[element] = true;
+      }
+    }
+
+    return duplicates.length ? duplicates[0] : null;
+  }
+
+  get radioCollections() {
+    let collection = [];
+
+    const checkedCount = this.optionsTarget.querySelectorAll("input[type='radio']:checked").length;
+
+    if (this.optionTargets.length != checkedCount) return collection;
+
+    this.optionTargets.forEach((el) => {
+      const checkedEl = el.querySelector("input[type='radio']:checked")
+
+      if (checkedEl?.dataset.variantIds) {
+        collection = [...collection, ...checkedEl.dataset.variantIds.split(",")];
+      }
+    })
+
+    return collection;
   }
 }
