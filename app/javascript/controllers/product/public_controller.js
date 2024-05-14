588225c358be374b9daa7c771b45e23ff184a7a7
felix1251 <domhack123@gmail.com>
2024-05-15 00:55:59 +0800

    public product radio toggle

--- app/javascript/controllers/product/public_controller.js
+++ app/javascript/controllers/product/public_controller.js
@@ -5,13 +5,31 @@ export default class extends Controller {
   static targets = ["options", "addToCartBtn", "buyNowBtn"]
 
   radioToggle(event) {
+    const radioName = event.target.name
+    const radioID = event.target.id
+
+    // clear radio not including the event element
+    this.setRadios(radioName, null, radioID)
+
     if (event.target.dataset.wasChecked == "true") {
-      event.target.checked = false
-      event.target.dataset.wasChecked = false
+      // set radio with the event element
+      this.setRadios(radioName, radioID, null)
       return
     }
 
     event.target.checked = true
     event.target.dataset.wasChecked = true
+  }
+
+  setRadios(name, includeId, excludeId) {
+    let query = `input[type="radio"][name="${name}"]`
+
+    if (includeId) query += `[id="${includeId}"]`
+    if (excludeId) query += `:not([id="${excludeId}"]`
+
+    this.optionsTarget.querySelectorAll(query).forEach((element) => {
+      element.checked = false
+      element.dataset.wasChecked = false
+    })
   }
 }
