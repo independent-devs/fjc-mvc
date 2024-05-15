dcbc565735222dee6cdc57049f0a7d96c9733d23
felix1251 <domhack123@gmail.com>
2024-05-15 22:12:56 +0800

    initialize price html

--- app/javascript/controllers/product/public_controller.js
+++ app/javascript/controllers/product/public_controller.js
@@ -2,7 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="product--public"
 export default class extends Controller {
-  static targets = ["option", "options", "addToCartBtn", "buyNowBtn"]
+  static targets = [
+    "price",
+    "option",
+    "options",
+    "addToCartBtn",
+    "buyNowBtn"
+  ]
+
+  connect() {
+    this.initPriceHTML = this.priceTarget.outerHTML
+  }
 
   radioToggle(event) {
     const radioName = event.target.name
