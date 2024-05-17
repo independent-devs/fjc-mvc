da8751e987cb974df19259b85e67c7d7d53c2c37
felix1251 <domhack123@gmail.com>
2024-05-18 02:53:58 +0800

    add order_id in cart indicator that already ordered

--- db/migrate/20240517165915_create_carts.rb
+++ db/migrate/20240517165915_create_carts.rb
@@ -4,9 +4,10 @@ class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
       t.uuid :uuid, null: false, default: 'gen_random_uuid()'
-      t.references :user, foreign_key: true
-      t.references :variant, null: false, foreign_key: true
       t.integer :qty, null: false, default: 1
+      t.references :variant, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :order, foreign_key: true
 
       t.timestamps
     end
