9cdc6c543b3fe5ca845fa28753358abba866306f
felix1251 <domhack123@gmail.com>
2024-05-17 17:03:23 +0800

    change omniauth controller

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -5,25 +5,33 @@ class OmniauthController < Devise::OmniauthCallbacksController
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in(@user)
-
-      redirect_to request.env['omniauth.params']['origin'] || root_path
-      set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+      success_redirect provider: 'Google'
     else
-      redirect_to(new_user_registration_path, error: 'There was a problem signing with Google')
+      error_redirect provider: 'Google'
     end
   end
 
   def facebook
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
+
     if @user.persisted?
       sign_in(@user)
-
-      redirect_to request.env['omniauth.params']['origin'] || root_path
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+      success_redirect provider: 'Facebook'
     else
       # Removing extra as it can overflow some session stores
       session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to(new_user_registration_path, error: 'There was a problem signing with Facebook')
+      error_redirect provider: 'Facebook'
     end
+  end
+
+  private
+
+  def success_redirect(provider:)
+    redirect_to request.env['omniauth.params']['origin'] || session['user_return_to'] || root_path
+    set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+  end
+
+  def error_redirect(provider:)
+    redirect_to(new_user_registration_path, error: "There was a problem signing with #{provider}")
   end
 end
