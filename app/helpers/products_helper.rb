5777af4ad88a7e74b680ce83a5c29bcee149dd27
felix1251 <domhack123@gmail.com>
2024-05-25 02:09:30 +0800

    add product helpers for public product

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ProductsHelper
+  def pub_product_stock_num(product)
+    return product.variants.not_master.stock_sum if product.has_variant
+
+    product.master_variant.count_on_hand
+  end
+
+  def pub_product_variant(product)
+    return '' if product.has_variant
+
+    product.master_variant.uuid
+  end
 end
