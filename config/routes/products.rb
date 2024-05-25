70fedae3961c5a502246b70d4d037c2ef6e564b9
felix1251 <domhack123@gmail.com>
2024-05-25 21:46:54 +0800

    simplify publlic product js

--- config/routes/products.rb
+++ config/routes/products.rb
@@ -2,5 +2,5 @@
 
 resources :products, param: :slug, only: %i[index show]
 
-post 'buy_now/:pid/:vid', to: 'products#buy_now', as: "buy_now"
-get 'variant_info/:pid/:vid', to: 'products#variant_info', as: "variant_info", defaults: { format: 'turbo_stream' }
+post 'products/buy_now/:pid/:vid', to: 'products#buy_now', as: "buy_now"
+get 'products/variant_info/:pid/:vid', to: 'products#variant_info', as: "variant_info", defaults: { format: 'turbo_stream' }
