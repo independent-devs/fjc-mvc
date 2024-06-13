71251096de8b1b4767021ccd3d8ba97c914526d6
felix1251 <domhack123@gmail.com>
2024-06-13 20:30:22 +0800

    update cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,16 +6,19 @@ class Ability
   def initialize(user, guest_session)
     if guest_session.present? # Guest
       # carts controller
-      can(:guest_update, Cart, guest_session:, order: nil) if user.blank?
-      can(:guest_destroy, Cart, guest_session:)
+      can(:guest_update, Cart, guest_session:, order: nil, user: nil) if user.blank?
+      can(:guest_destroy, Cart, guest_session:, order: nil)
     end
 
     return if user.blank? # Authenticated
 
     # carts controller
+    if guest_session.present?
+      can(:sync, Cart, guest_session:, order: nil, user: nil)
+      can(:sync_all, Cart)
+    end
+
     can(:update, Cart, user:, order: nil)
     can(:destroy, Cart, user:)
-    can(:sync, Cart, user: nil, guest_session:, order: nil)
-    can(:sync_all, Cart) if guest_session.present?
   end
 end
