9f0a37741cd015939b7222d2e9de1ecfdfcc638b
felix1251 <domhack123@gmail.com>
2024-06-18 02:59:57 +0800

    order ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,23 +8,30 @@ class Ability
 
     return if user.blank?
 
+    # Cart
+    can(:index, Cart, user:, order: nil)
+    can(:index, Cart, guest_session:, order: nil) if guest_session.present?
+    can(%i[update destroy], Cart, user:, order: nil)
+
     if guest_session.present?
       can(:sync, Cart, guest_session:, order: nil, user: nil)
       can(:sync_all, Cart)
     end
 
-    # User OR guest cart
-    can(:index, Cart, user:, order: nil)
-    can(:index, Cart, guest_session:, order: nil) if guest_session.present?
-
-    can(%i[update destroy], Cart, user:, order: nil)
+    # Order
+    can(:read, Order, user:)
+    can(:cancel, Order, user:)
   end
 
   private
 
   def guest(guest_session)
+    # Cart
     can(:index, Cart, guest_session:, user: nil, order: nil)
-    can(%i[update destroy], Cart, guest_session:, order: nil, user: nil)
-    can(%i[update], Order, guest_session:, user: nil)
+    can(%i[update destroy], Cart, guest_session:, user: nil, order: nil)
+
+    # Order
+    can(:read, Order, guest_session:, user: nil)
+    can(:cancel, Order, guest_session:, user: nil)
   end
 end
