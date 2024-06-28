3b34e5a110ade8dbf5145ae961327bc2c65013d5
felix1251 <domhack123@gmail.com>
2024-06-28 22:35:08 +0800

    type helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,28 +5,21 @@ module ApplicationHelper
   extend T::Sig
   include Pagy::Frontend
 
-  sig do
-    params(
-      price: T.any(Integer, Float, BigDecimal),
-      unit: T.nilable(String)
-    ).returns(T.nilable(String))
-  end
-  def price_with_currency(price, unit = nil)
-    ActionController::Base
+  sig { params(price: T.any(Integer, Float, BigDecimal), unit: String).returns(String) }
+  def price_with_currency(price, unit = '')
+    T.must(ActionController::Base
       .helpers
       .number_to_currency(
         price,
         precision: 2,
         strip_insignificant_zeros: true,
         unit:
-      )
+      ))
   end
 
   sig do
-    params(
-      price: T.any(Integer, Float, BigDecimal),
-      qty: Integer
-    ).returns(T.any(Integer, Float, BigDecimal))
+    params(price: T.any(Integer, Float, BigDecimal), qty: Integer)
+      .returns(T.any(Integer, Float, BigDecimal))
   end
   def total_price_calc(price:, qty: 1)
     price * qty
