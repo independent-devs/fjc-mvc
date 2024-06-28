d165a22614ba216c29005bee28fe49834a251254
felix1251 <domhack123@gmail.com>
2024-06-28 22:20:57 +0800

    type helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,33 @@
 # frozen_string_literal: true
+# typed: true
 
 module ApplicationHelper
+  extend T::Sig
   include Pagy::Frontend
 
+  sig do
+    params(
+      price: T.any(Integer, Float, BigDecimal),
+      unit: T.nilable(String)
+    ).returns(T.nilable(String))
+  end
   def price_with_currency(price, unit = nil)
-    number_to_currency(price, precision: 2, strip_insignificant_zeros: true, unit:)
+    ActionController::Base
+      .helpers
+      .number_to_currency(
+        price,
+        precision: 2,
+        strip_insignificant_zeros: true,
+        unit:
+      )
   end
 
+  sig do
+    params(
+      price: T.any(Integer, Float, BigDecimal),
+      qty: Integer
+    ).returns(T.any(Integer, Float, BigDecimal))
+  end
   def total_price_calc(price:, qty: 1)
     price * qty
   end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
+# typed: true
 
 module ProductsHelper
+  extend T::Sig
+
   def currency_list
     Money::Currency.table.map do |cu|
       ["#{cu[1][:iso_code]} (#{cu[1][:symbol]})", cu[1][:iso_code]]
     end
   end
 
+  sig { params(product: Product).returns(T.any(Integer, Float, BigDecimal)) }
   def pub_stock_num(product)
-    return product.variants.not_master.stock_sum if product.has_variant
+    return product.variants.not_master.sum(:count_on_hand) if product.has_variant
 
-    product.master_variant.count_on_hand
+    T.must(T.must(product.master_variant).count_on_hand)
   end
 
+  sig { params(product: Product).returns(String) }
   def pub_variant_uuid(product)
     return '' if product.has_variant
 
-    product.master_variant.uuid
+    T.must(product.master_variant).uuid
   end
 end
