4b734109adac2298d45d388d8ec3a74f9ced8786
felix1251 <domhack123@gmail.com>
2024-07-02 17:58:19 +0800

    navbar account dropdown

--- vendor/javascript/@stimulus-components--dropdown.js
+++ vendor/javascript/@stimulus-components--dropdown.js
@@ -1,2 +1,19 @@
-import{Controller as t}from"@hotwired/stimulus";import{useTransition as e}from"stimulus-use";const s=class _Dropdown extends t{connect(){e(this,{element:this.menuTarget})}toggle(){this.toggleTransition()}hide(t){!this.element.contains(t.target)&&!this.menuTarget.classList.contains("hidden")&&this.leave()}};s.targets=["menu"];let i=s;export{i as default};
+import { Controller as t } from "@hotwired/stimulus";
+import { useTransition as e } from "stimulus-use";
 
+const s = class _Dropdown extends t {
+  connect() {
+    e(this, { element: this.menuTarget });
+  }
+  toggle() {
+    this.toggleTransition();
+  }
+  hide(t) {
+    !this.element.contains(t.target) &&
+      !this.menuTarget.classList.contains("hidden") &&
+      this.leave();
+  }
+};
+s.targets = ["menu"];
+let i = s;
+export { i as default };
