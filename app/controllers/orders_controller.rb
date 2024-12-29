430ac01781df5a892bd8a9f9c0cbac278381903a
felix1251 <felixabacajen17@gmail.com>
2024-12-30 02:21:31 +0800

    feat: user order controller change

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,8 @@ class OrdersController < BaseController
   load_and_authorize_resource
 
   def index
-    @orders = Order.accessible_by(current_ability)
-    @orders = @orders.where(order_status: { name: params[:status] }).joins(:order_status) if params[:status].present?
+    @orders = Order.with_status.accessible_by(current_ability)
+    @orders = @orders.where(order_statuses: { name: params[:status] }) if params[:status].present?
   end
 
   def show; end
