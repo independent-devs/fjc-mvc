b24228a6b2edfd18cfe21f38d3401b8b7d8a8b82
felix1251 <felixabacajen17@gmail.com>
2024-12-31 20:45:50 +0800

    feat: adjust monet rails config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,24 +3,17 @@
 
 module ApplicationHelper
   extend T::Sig
+
+  # Concerns
   include Pagy::Frontend
 
   sig { params(price: T.any(Integer, Float, BigDecimal), unit: String).returns(String) }
   def price_with_currency(price, unit = '')
-    T.must(ActionController::Base
-      .helpers
-      .number_to_currency(
-        price,
-        precision: 2,
-        strip_insignificant_zeros: true,
-        unit:
-      ))
+    T.must(ActionController::Base.helpers
+      .number_to_currency(price, precision: 2, strip_insignificant_zeros: true, unit:))
   end
 
-  sig do
-    params(price: T.any(Integer, Float, BigDecimal), qty: Integer)
-      .returns(T.any(Integer, Float, BigDecimal))
-  end
+  sig { params(price: T.any(Integer, Float, BigDecimal), qty: Integer).returns(T.any(Integer, Float, BigDecimal)) }
   def total_price_calc(price:, qty: 1)
     price * qty
   end
