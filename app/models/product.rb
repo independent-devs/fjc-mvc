dbb55add3b84ff908cfbf702e2f521d258832f83
felix1251 <felixabacajen17@gmail.com>
2025-01-01 22:58:10 +0800

    feat: change layout maw width

--- app/models/product.rb
+++ app/models/product.rb
@@ -37,7 +37,7 @@ class Product < ApplicationRecord
   accepts_nested_attributes_for :master_variant, :description, :product_category, :seo
 
   # Scopes
-  scope :sort_by_latest, -> { order(id: :desc) }
+  scope :sort_by_latest, -> { order(created_at: :desc) }
   scope :base_on_date, lambda { |now = Date.current|
     where(available_on: ..now)
       .where('discontinue_on IS NULL OR discontinue_on <= ?', now)
