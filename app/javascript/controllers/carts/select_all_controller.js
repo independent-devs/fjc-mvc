2bdf87f73f3e2e355faad472140d20696db0915c
felix1251 <felixabacajen17@gmail.com>
2025-01-07 00:30:13 +0800

    feat: cart bulk delete and select all checkbox js

--- app/javascript/controllers/carts/select_all_controller.js
+++ app/javascript/controllers/carts/select_all_controller.js
@@ -2,7 +2,7 @@ import CheckboxSelectAll from "@stimulus-components/checkbox-select-all";
 
 // Connects to data-controller="carts--select-all"
 export default class extends CheckboxSelectAll {
-  static targets = ["selected", "total"];
+  static targets = ["selected", "total", "bulkDeleteBtn", "selectAll", "activeCount"];
 
   connect() {
     this.initTotalEl = this.totalTarget.innerHTML;
@@ -10,11 +10,13 @@ export default class extends CheckboxSelectAll {
 
   refresh() {
     super.refresh();
+    this.selectAllRefresh();
     this.displaySelected();
   }
 
-  toggle(e) {
-    super.toggle(e);
+  toggle(event) {
+    super.toggle(event);
+    this.selectAllRefresh();
     this.displaySelected();
   }
 
@@ -23,12 +25,11 @@ export default class extends CheckboxSelectAll {
 
     if (!this.checked.length) {
       this.totalTarget.innerHTML = this.initTotalEl || this.totalTarget.innerHTML;
+      this.selectAllTarget.checked = false;
       return;
     }
 
-    const params = "?" + this.checked.map((el) => `ids[]=` + el.dataset.cartId).join("&");
-
-    fetch(this.element.dataset.totalUrl + params, {
+    fetch(this.element.dataset.totalUrl + "?" + this.checked.map((el) => `ids[]=` + el.dataset.cartId).join("&"), {
       method: "GET",
       headers: {
         Accept: "text/vnd.turbo-stream.html",
@@ -40,5 +41,29 @@ export default class extends CheckboxSelectAll {
         if (!this.checked.length) return;
         return Turbo.renderStreamMessage(html);
       });
+  }
+
+  bulkDelete() {
+    if (!this.checked.length) return;
+
+    fetch(this.element.dataset.bulkDeleteUrl + "?" + this.checked.map((el) => `ids[]=` + el.dataset.cartId).join("&"), {
+      method: "GET",
+      headers: {
+        Accept: "text/vnd.turbo-stream.html",
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').getAttribute("content"),
+      },
+    })
+      .then((res) => res.text())
+      .then((html) => Turbo.renderStreamMessage(html));
+  }
+
+  selectAllRefresh() {
+    this.activeCountTarget.innerHTML = `Select All (${this.activeCheckbox.length})`;
+    this.selectAllTarget.checked = this.activeCheckbox.length == this.checked.length;
+  }
+
+  selectAllToggle(event) {
+    this.toggle(event);
+    super.refresh();
   }
 }
